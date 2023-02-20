3e861f2f64bb09ce69d6baf0531beed0b24d512f
joern-kalz <joern.kalz@gmail.com>
2023-02-20 10:21:39 +0100

    Adds snake scaffold

---
+++ src/snake.rs
@@ -0,0 +1,20 @@
+struct Snake {  
+    pub segments: Vec<Point2D>,
+}
+
+struct Point2D(i32, i32);
+
+enum Direction {
+    Left,
+    Right,
+}
+
+impl Snake {
+    fn move_forward(& mut self) {
+      
+    }
+
+    fn turn(& mut self, direction: Direction) {
+
+    }
+}
