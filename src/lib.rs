276030c3867280472833dc1e5935d59a1cd1a239
joern-kalz <joern.kalz@gmail.com>
2023-03-04 09:48:01 +0100

    Setup pixel based game

--- src/lib.rs
+++ src/lib.rs
@@ -1,100 +1,46 @@
-use std::cmp;
-use std::f64;
 use wasm_bindgen::prelude::*;
-mod snake;
-use snake::{Point2D, Snake};
+use wasm_bindgen::Clamped;
+use web_sys::{CanvasRenderingContext2d, ImageData};
 
-fn window() -> web_sys::Window {
-    web_sys::window().expect("no global `window` exists")
-}
-
-fn document() -> web_sys::Document {
-    window()
-        .document()
-        .expect("should have a document on window")
-}
-
-fn canvas() -> web_sys::HtmlCanvasElement {
-    document()
-        .get_element_by_id("canvas")
-        .expect("should have an element with id 'canvas' in the document")
-        .dyn_into::<web_sys::HtmlCanvasElement>()
-        .map_err(|_| ())
-        .expect("canvas should be a HtmlCanvasElement")
-}
+const SCREEN_WIDTH: usize = 30;
+const SCREEN_HEIGHT: usize = 30;
+const BYTES_PER_PIXEL: usize = 4;
+const SCREEN_SIZE: usize = SCREEN_WIDTH * SCREEN_HEIGHT * BYTES_PER_PIXEL;
 
 #[wasm_bindgen]
 pub struct Game {
-    snake: Snake,
+    screen: [u8; SCREEN_SIZE],
 }
 
 #[wasm_bindgen]
 impl Game {
     pub fn new() -> Game {
         Game {
-            snake: Snake::new(),
+            screen: [155; SCREEN_SIZE],
         }
     }
 
-    pub fn tick(&mut self) -> bool {
-        self.snake.move_forward()
+    pub fn width() -> usize {
+        SCREEN_WIDTH
     }
 
-    pub fn render(&self) {
-        let canvas = canvas();
-
-        let context = canvas
-            .get_context("2d")
-            .unwrap()
-            .unwrap()
-            .dyn_into::<web_sys::CanvasRenderingContext2d>()
-            .unwrap();
-
-        context.clear_rect(0.0, 0.0, canvas.width() as f64, canvas.height() as f64);
-
-        context.set_fill_style(&JsValue::from_str("green"));
-
-        for segment in self.snake.segments() {
-            let v0 = &segment.0;
-            let v1 = &segment.1;
-
-            let x = if v0.x != v1.x {
-                cmp::min(v0.x, v1.x)
-            } else {
-                v0.x - 2
-            };
-            let y = if v0.y != v1.y {
-                cmp::min(v0.y, v1.y)
-            } else {
-                v0.y - 2
-            };
-            let w = cmp::max(i32::abs_diff(v0.x, v1.x), 4);
-            let h = cmp::max(i32::abs_diff(v0.y, v1.y), 4);
-
-            context.set_fill_style(&JsValue::from_str("green"));
-            context.fill_rect(x as f64, y as f64, w as f64, h as f64);
-            context.begin_path();
-            context
-                .arc(v0.x as f64, v0.y as f64, 2.0, 0.0, f64::consts::PI * 2.0)
-                .unwrap();
-            context
-                .arc(v1.x as f64, v1.y as f64, 2.0, 0.0, f64::consts::PI * 2.0)
-                .unwrap();
-            context.set_fill_style(&JsValue::from_str("red"));
-            context
-                .arc(
-                    self.snake.food.x as f64,
-                    self.snake.food.y as f64,
-                    2.0,
-                    0.0,
-                    f64::consts::PI * 2.0,
-                )
-                .unwrap();
-            context.fill();
-        }
+    pub fn height() -> usize {
+        SCREEN_HEIGHT
     }
 
-    pub fn turn(&mut self) {
-        self.snake.turn(snake::TurnDirection::Left)
+    pub fn tick(&mut self) {}
+
+    pub fn render(&mut self, ctx: &CanvasRenderingContext2d) {
+        let data = ImageData::new_with_u8_clamped_array_and_sh(
+            Clamped(&self.screen),
+            SCREEN_WIDTH.try_into().unwrap(),
+            SCREEN_HEIGHT.try_into().unwrap(),
+        )
+        .expect("should create ImageData from array");
+
+        ctx.put_image_data(&data, 0.0, 0.0)
+            .expect("should write array to context");
     }
+
+    pub fn turn(&mut self) {}
 }
